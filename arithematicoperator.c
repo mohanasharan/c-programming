bda9b9b5aa119ea833d458188e4e33e09b97f2d9
mohanasharan <95616336+mohanasharan@users.noreply.github.com>
2021-12-06 15:14:14 +0530

    Create arithematicoperator.c

---
+++ arithematicoperator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{clrscr();
+int a,b,c;
+printf("enter the value of a and b");
+scanf("%d",&a);
+scanf("%d",&b);
+c=a+b;
+printf("\nsum=%d",c);
+printf("\ndifference=%d",a-b);
+printf("\nmultiplication=%d",a*b);
+printf("\ndivision=%d",a/b);
+printf("\nreminder=%d",a%b);
+getch();
+}
